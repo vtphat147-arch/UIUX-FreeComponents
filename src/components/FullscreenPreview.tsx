0663142da331c51ac30c723bb97cb9003cb4c321
h3canlao <vtp336@gmail.com>
2025-12-30 19:30:37 +0700

    Clean up: Remove README.md and update frontend files

--- src/components/FullscreenPreview.tsx
+++ src/components/FullscreenPreview.tsx
@@ -145,3 +145,5 @@ const FullscreenPreview = ({
 export default FullscreenPreview
 
 
+
+
--- src/hooks/useVipStatus.ts
+++ src/hooks/useVipStatus.ts
@@ -59,3 +59,5 @@ export const useVipStatus = () => {
   return { vipStatus, loading, refreshStatus }
 }
 
+
+
